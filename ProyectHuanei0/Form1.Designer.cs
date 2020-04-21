666e78bdd38385bbe2c0644df0ffe184ba57db22
GabiCrisStevens <gabicristino@gmail.com>
2020-04-20 23:51:48 -0300

    ProjectHuanei00

---
+++ ProyectHuanei0/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ProyectHuanei0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbNacimiento = new System.Windows.Forms.Label();
+            this.lbDireccion = new System.Windows.Forms.Label();
+            this.lbApellido = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.dtmNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lbDNI = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.dtvTabla = new System.Windows.Forms.DataGridView();
+            this.btEliminar = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dtvTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(38, 31);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(44, 13);
+            this.lbNombre.TabIndex = 0;
+            this.lbNombre.Text = "Nombre";
+            // 
+            // lbNacimiento
+            // 
+            this.lbNacimiento.AutoSize = true;
+            this.lbNacimiento.Location = new System.Drawing.Point(38, 116);
+            this.lbNacimiento.Name = "lbNacimiento";
+            this.lbNacimiento.Size = new System.Drawing.Size(106, 13);
+            this.lbNacimiento.TabIndex = 2;
+            this.lbNacimiento.Text = "Fecha de nacimiento";
+            // 
+            // lbDireccion
+            // 
+            this.lbDireccion.AutoSize = true;
+            this.lbDireccion.Location = new System.Drawing.Point(38, 152);
+            this.lbDireccion.Name = "lbDireccion";
+            this.lbDireccion.Size = new System.Drawing.Size(52, 13);
+            this.lbDireccion.TabIndex = 3;
+            this.lbDireccion.Text = "Direccion";
+            // 
+            // lbApellido
+            // 
+            this.lbApellido.AutoSize = true;
+            this.lbApellido.Location = new System.Drawing.Point(39, 76);
+            this.lbApellido.Name = "lbApellido";
+            this.lbApellido.Size = new System.Drawing.Size(44, 13);
+            this.lbApellido.TabIndex = 4;
+            this.lbApellido.Text = "Apellido";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(201, 31);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(268, 20);
+            this.txtNombre.TabIndex = 5;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(201, 75);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(268, 20);
+            this.txtApellido.TabIndex = 6;
+            this.txtApellido.TextChanged += new System.EventHandler(this.txtApellido_TextChanged);
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(201, 152);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(268, 20);
+            this.txtDireccion.TabIndex = 7;
+            this.txtDireccion.TextChanged += new System.EventHandler(this.txtDireccion_TextChanged);
+            // 
+            // dtmNacimiento
+            // 
+            this.dtmNacimiento.Location = new System.Drawing.Point(201, 116);
+            this.dtmNacimiento.Name = "dtmNacimiento";
+            this.dtmNacimiento.Size = new System.Drawing.Size(268, 20);
+            this.dtmNacimiento.TabIndex = 8;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(520, 123);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(520, 152);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lbDNI
+            // 
+            this.lbDNI.AutoSize = true;
+            this.lbDNI.Location = new System.Drawing.Point(41, 187);
+            this.lbDNI.Name = "lbDNI";
+            this.lbDNI.Size = new System.Drawing.Size(26, 13);
+            this.lbDNI.TabIndex = 12;
+            this.lbDNI.Text = "DNI";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(201, 187);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(268, 20);
+            this.txtDNI.TabIndex = 13;
+            this.txtDNI.TextChanged += new System.EventHandler(this.txtDNI_TextChanged);
+            // 
+            // dtvTabla
+            // 
+            this.dtvTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtvTabla.Location = new System.Drawing.Point(41, 238);
+            this.dtvTabla.Name = "dtvTabla";
+            this.dtvTabla.Size = new System.Drawing.Size(554, 192);
+            this.dtvTabla.TabIndex = 14;
+            this.dtvTabla.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtvTabla_CellClick);
+            this.dtvTabla.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtvTabla_CellContentClick);
+            // 
+            // btEliminar
+            // 
+            this.btEliminar.Location = new System.Drawing.Point(520, 181);
+            this.btEliminar.Name = "btEliminar";
+            this.btEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btEliminar.TabIndex = 15;
+            this.btEliminar.Text = "Eliminar";
+            this.btEliminar.UseVisualStyleBackColor = true;
+            this.btEliminar.Click += new System.EventHandler(this.btEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 450);
+            this.Controls.Add(this.btEliminar);
+            this.Controls.Add(this.dtvTabla);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.lbDNI);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dtmNacimiento);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lbApellido);
+            this.Controls.Add(this.lbDireccion);
+            this.Controls.Add(this.lbNacimiento);
+            this.Controls.Add(this.lbNombre);
+            this.Name = "Form1";
+            this.Text = "Formulario";
+            ((System.ComponentModel.ISupportInitialize)(this.dtvTabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbNacimiento;
+        private System.Windows.Forms.Label lbDireccion;
+        private System.Windows.Forms.Label lbApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.DateTimePicker dtmNacimiento;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lbDNI;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.DataGridView dtvTabla;
+        private System.Windows.Forms.Button btEliminar;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ProyectHuanei0/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProyectHuanei0
+{
+    public partial class Form1 : Form
+    {
+        public int IDCliente;
+        public Form1()
+        {
+            IDCliente = 0;
+            InitializeComponent();
+            reestablecer();
+        }
+
+        private class Cliente
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public string Fecha_Nac { get; set; }
+            public string Direccion { get; set; } 
+        }
+
+        SqlConnection conectar = new SqlConnection(" Fuente de datos GABYPC\\GABYPC; Catálogo inicial = Taller; Seguridad integrada = verdadero; ");
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtApellido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDireccion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDNI_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string strNombre, strApellido , strNacimiento , strDireccion;
+            strNombre = txtNombre.Text;
+            strApellido = txtApellido.Text;
+            strNacimiento = dtmNacimiento.Value.Year.ToString() + '/' + dtmNacimiento.Value.Month.ToString() + '/' + dtmNacimiento.Value.Day.ToString();
+            strDireccion = txtDireccion.Text;
+
+
+            if (IDCliente.Equals(0))
+            {
+
+                List<Cliente> lista = new List<Cliente>();
+
+                string consulta = string.Format("Insert into clientes (Nombre, Apellido, Nacimiento, Direccion) values ('{0}','{1}','{2}','{3}')",
+                   strNombre, strApellido, strNacimiento, strDireccion);
+
+                SqlCommand comando = new SqlCommand(consulta, conectar);
+
+                conectar.Open();
+                comando.ExecuteNonQuery();
+                conectar.Close();
+
+                MessageBox.Show("Cliente " + txtNombre.Text + " " + txtApellido.Text + " registrado con exito", "Registro Exitoso");
+
+            }
+
+
+            else
+            {
+
+                string consulta = string.Format("Update clientes set Nombre = '{0}', Apellido =  '{1}' , Nacimiento =  '{2}', Direccion = '{3}' where IDCliente =  '{4}'",
+               strNombre, strApellido, strNacimiento, strDireccion, IDCliente);
+
+                SqlCommand comando = new SqlCommand(consulta, conectar);
+
+                conectar.Open();
+                comando.ExecuteNonQuery();
+                conectar.Close();
+
+                MessageBox.Show("Cliente " + txtNombre.Text + " " + txtApellido.Text + " modificado con exito", "Modificación Exitosa");
+
+            }
+
+            reestablecer();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            reestablecer();
+        }
+
+        private void btEliminar_Click(object sender, EventArgs e)
+        {
+            if (IDCliente.Equals(0))
+            {
+                MessageBox.Show("Seleccione un cliente", "Atención");
+            }
+            else
+            {
+                if (MessageBox.Show("¿Está seguro que desea eliminar el cliente?", "Eliminar Cliente", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+
+                    String consulta = "DELETE from clientes where IDCliente= '" + IDCliente + "'";
+                    SqlCommand comando = new SqlCommand(consulta, conectar);
+
+                    conectar.Open();
+                    comando.ExecuteNonQuery();
+                    conectar.Close();
+
+                    reestablecer();
+                }
+            }
+        }
+
+        private void dtvTabla_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dtvTabla_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            IDCliente = Convert.ToInt32(dtvTabla.CurrentRow.Cells[0].Value);
+            txtNombre.Text = Convert.ToString(dtvTabla.CurrentRow.Cells[1].Value);
+            txtApellido.Text = Convert.ToString(dtvTabla.CurrentRow.Cells[2].Value);
+
+            dtmNacimiento.Value = Convert.ToDateTime(dtvTabla.CurrentRow.Cells[3].Value);
+            txtDireccion.Text = Convert.ToString(dtvTabla.CurrentRow.Cells[4].Value);
+
+            btnGuardar.Text = "Guardar";
+        }
+
+        public void reestablecer()
+        {
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            dtmNacimiento.Value = System.DateTime.Today;
+            txtDireccion.Text = "";
+            IDCliente = 0;
+            btnGuardar.Text = "Nuevo";
+
+            mostrarRegistro();
+        }
+
+        private void mostrarRegistro()
+        {
+            List<Cliente> lista = new List<Cliente>();
+
+            
+            String consulta = "SELECT IDCliente, Nombre, Apellido, Nacimiento, Direccion FROM clientes";
+            SqlCommand comando = new SqlCommand(consulta, conectar);
+
+            conectar.Open();
+            SqlDataReader reader = comando.ExecuteReader();
+
+            while (reader.Read()) 
+            {
+                Cliente pCliente = new Cliente();
+                pCliente.Id = reader.GetInt32(0);
+                pCliente.Nombre = reader.GetString(1);
+                pCliente.Apellido = reader.GetString(2);
+                pCliente.Fecha_Nac = reader.GetString(3);
+                pCliente.Direccion = reader.GetString(4);
+
+
+                lista.Add(pCliente);
+            }
+
+            conectar.Close();
+            btnGuardar.DataSource = lista;
+
+        }
+    }
+}
